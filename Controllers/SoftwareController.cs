6fce092a6189d8c6f1e6b4b4d53a0856893d346a
Harold Glenn Minerva <hgminerva@gmail.com>
2016-02-22 14:25:24 +0800

    Initial Commit

---
+++ Controllers/SoftwareController.cs
@@ -0,0 +1,168 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Mvc;
+
+namespace HRISVSC.Controllers
+{
+    public class SoftwareController : Controller
+    {
+        public IActionResult MainMenu()
+        {
+            return View();
+        }
+        ///////////////////
+        //Setup under Menu
+        ///////////////////
+        
+        public IActionResult Employee()
+        {
+            
+            return View();
+        }
+        public IActionResult ShiftCode()
+        {
+            
+            return View();
+        }
+        public IActionResult Company()
+        {
+            
+            return View();
+        }
+        public IActionResult DayType()
+        {
+            
+            return View();
+        }
+        public IActionResult MandatoryTaxTables()
+        {
+            
+            return View();
+        }
+        public IActionResult OtherIncome()
+        {
+            
+            return View();
+        }
+        public IActionResult SystemTables()
+        {
+            
+            return View();
+        }
+        public IActionResult OtherDeduction()
+        {
+            
+            return View();
+        }
+        public IActionResult Users()
+        {
+            
+            return View();
+        }
+        public IActionResult ChangeShiftCode()
+        {
+            
+            return View();
+        }
+        public IActionResult Settings()
+        {
+            
+            return View();
+        }
+        ///////////////////////////
+        // Activities Under Main Menu
+        ///////////////////////
+        public IActionResult DTR()
+        {
+            
+            return View();
+        }
+        public IActionResult PayrollOtherIncome()
+        {
+            
+            return View();
+        }
+        public IActionResult PayrollOtherDeduction()
+        {
+            
+            return View();
+        }
+        public IActionResult LeaveApplication()
+        {
+            
+            return View();
+        }
+        public IActionResult LastWithholdingTax()
+        {
+            
+            return View();
+        }
+        public IActionResult OvertimeApplication()
+        {
+            
+            return View();
+        }
+        public IActionResult Loans()
+        {
+            
+            return View();
+        }
+        public IActionResult Payroll()
+        {
+            
+            return View();
+        }
+        
+        ////////////////////////////////////////////////////////////////
+        //Reports Under Main Menu
+        //////////////////////////////////////////////////////////////////////
+        
+        public IActionResult DTRReports()
+        {
+            
+            return View();
+        }
+        public IActionResult LeaveReports()
+        {
+            
+            return View();
+        }
+        public IActionResult PayrollReports()
+        {
+            
+            return View();
+        }
+        public IActionResult LoanReports()
+        {
+            
+            return View();
+        }
+        public IActionResult MandatoryReports()
+        {
+            
+            return View();
+        }
+        public IActionResult AccountingReports()
+        {
+            
+            return View();
+        }
+        public IActionResult WithholdingTaxReports()
+        {
+            
+            return View();
+        }
+        public IActionResult ATMBank()
+        {
+            
+            return View();
+        }
+        public IActionResult Demographics()
+        {
+            
+            return View();
+        }
+    }
+}
